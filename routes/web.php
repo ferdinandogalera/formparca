ddf6141a5a335ee6dbcd9f97487c18bc94f97385
Ferdinando <ferdinandogalera@gmail.com>
2021-12-29 10:22:00 -0300

    Aponta pra MAILVIEW

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,14 @@ use App\Http\Controllers\MailController;
 
 
 
-/*
-Route::get('/', function () {
-    return view('site.home');
-})->name('site.home');
-
-Route::view('form', 'site.home');
-*/
-
-
-
 Route::get('mail', [MailController::class, 'mailView'])->name('mailView');
 Route::post('send-mail', [MailController::class, 'mailSend'])->name('mailSend');
 
 Route::get('search', 'AutoCompleteController@index');
 Route::get('autocomplete', 'AutoCompleteController@search');
 
+Route::get('/', function () {
+    return view('mailView');
+});
 
 ?>
